752ed771b455d2540106a58fc2a5a8b93df38d2f
Nicolas <nicolas.nguyen@tomexplore.com>
2026-04-22 11:33:49 +0200

    fix(provider): force s3 path-style addressing for localstack compatibility

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ provider "aws" {
   skip_credentials_validation = var.use_localstack
   skip_metadata_api_check     = var.use_localstack
   skip_requesting_account_id  = var.use_localstack
+  s3_use_path_style           = var.use_localstack
 
   dynamic "endpoints" {
     for_each = var.use_localstack ? [1] : []
